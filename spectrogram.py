9191bb808ff6417bf0081a5ab7e44b260cb8bdf5
Andrios Robert Silva Pereira <andriosr@gmail.com>
2018-06-23 17:58:09 -0300

    initial commit

---
+++ spectrogram.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import os
+import wave
+import pylab
+
+def graph_spectrogram_custom(wav_file):
+    sound_info, frame_rate = get_wav_info(wav_file)
+    pylab.figure(num=None, figsize=(19, 12))
+    pylab.subplot(111)
+    pylab.title('spectrogram of %r' % wav_file)
+    pylab.specgram(sound_info, Fs=frame_rate)
+    pylab.savefig('spectrogram.png')
+    return sound_info
+
+def get_wav_info_custom(wav_file):
+    wav = wave.open(wav_file, 'r')
+    frames = wav.readframes(-1)
+    sound_info = pylab.fromstring(frames, 'int16')
+    frame_rate = wav.getframerate()
+    wav.close()
+    return sound_info
